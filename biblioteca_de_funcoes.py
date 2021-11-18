e8a884f6a9d9d1e66a5a72ead5c16f15acb21cef
Gabriel Santos <82888506+GabrielSantosCruz@users.noreply.github.com>
2021-11-18 11:42:53 -0300

    First Commit

---
+++ biblioteca_de_funcoes.py
@@ -0,0 +1,21 @@
+def validation_str(var, palavras_chave): # validar strings de cadastro com S ou N ou M e F
+    while var.upper() not in palavras_chave or len(var) == 0:
+        var = input(f'Erro, digite apenas [{palavras_chave[0]}/{palavras_chave[1]}]: ')
+    return var.upper()
+
+def validation_int(var): # validar números inteiros
+    while not var.isdigit() or len(var) == 0:
+        var = input('Erro, Digite apenas números: ')
+    return int(var)
+
+def cadastrar_lista(lista): # usa uma lista com as opções para transformar em opções númericas para cadastrar
+
+    for numero, nome in enumerate(lista): # transforma em opção
+        print(f'{numero} - {nome}')
+    
+    cadastro = validation_int(input("Digite a opção: "))
+    for numero, nome in enumerate(lista): # compara a opção digitada com os da lista
+        if cadastro == numero:
+            cadastro = nome  
+
+    return cadastro
