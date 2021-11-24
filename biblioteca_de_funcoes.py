668499c28de59e27cdefa4f583a279cf879ca389
Gabriel Santos <82888506+GabrielSantosCruz@users.noreply.github.com>
2021-11-24 14:52:14 -0300

    Changes in the visual of the funtions

--- biblioteca_de_funcoes.py
+++ biblioteca_de_funcoes.py
@@ -13,14 +13,24 @@ def validation_cpf(cpf): # validar se o cpf digitado é considerado válido
         cpf = input("Erro! Cpf inválido, digite novamente: ")
     return cpf
 
+def validation_limited_int(limitation, num):
+    while not num.isdigit or int(num) > limitation:
+        num = input("Erro, digite um número válido: ")
+    return int(num)
 def cadastrar_lista(lista): # usa uma lista com as opções para transformar em opções númericas para cadastrar
-
     for numero, nome in enumerate(lista): # transforma em opção
-        print(f'{numero} - {nome}')
+        print(f'[{numero}] - {nome}')
     
-    cadastro = validation_int(input("Digite a opção: "))
+    cadastro = validation_limited_int(len(lista), input("Digite a opção: "))
     for numero, nome in enumerate(lista): # compara a opção digitada com os da lista
         if cadastro == numero:
             cadastro = nome  
 
     return cadastro
+
+def printar_cadastros(nome_arquivo): # ler os arquivos cadastrados
+    import jsonpickle as jp
+    with open(nome_arquivo, 'r') as arquivo:
+        for cadastro in arquivo:
+            atleta = jp.decode(cadastro)
+            print(f'{atleta.cpf}\n{atleta.name}\n{atleta.age}\n{atleta.gender}\n{atleta.paralisy}\n{atleta.sports_quant}\n{atleta.medal}\n{atleta.modalidade}')
--- main.py
+++ main.py
@@ -6,34 +6,38 @@ sports = ['Atletismo', 'Badminton', 'Basquetebol em cadeira de rodas', 'Bocha', 
 deficiency = ['Deficiência mental', 'Visual', 'Amputação', 'Deficiência física']
 modalidades = dados = []
 medal_gold = medal_silver = medal_bronze = 0
-print('''OPÇÕES:
-1 - Cadastrar
-2 - Editar Cadastro
-3 - Excluir Cadastro''')
+medal = {}
 
-while True:
-    cpf = validation_cpf(input("Digite o cpf do Atleta: "))
-    name = input("Nome do Atleta: ")
-    age = validation_int(input("Digite a idade do Atleta: "))
-    gender = validation_str(input("Sexo do Atleta: [M/F] "), 'MF')
-    print("Digite apenas o número da opção correspondente a dezficiência que o Atleta possui")
-    paralisy = cadastrar_lista(deficiency)
-    covid = validation_str(input("Foi diagnosticado com Covid-19?: [S/N] "), 'SN')
-    sports_quant = validation_int(input("De quantas modalidades o Atleta paticipou: "))
+# inicio do programa
+while True: # depois tirar esse True aqui klsadjksa
+    print('''OPÇÕES:
+[1] - Cadastrar
+[2] - Editar Cadastro
+[3] - Excluir Cadastro
+[4] - Gerar Relatório''')
+    #inputs
+    option = validation_limited_int(4,(input("Digite a opção: ")))
+    if option == 1:
+        cpf = validation_cpf(input("Digite o cpf do Atleta: "))
+        name = input("Nome do Atleta: ")
+        age = validation_int(input("Digite a idade do Atleta: "))
+        gender = validation_str(input("Sexo do Atleta: [M/F] "), 'MF')
+        print("Digite apenas o número da opção correspondente a dezficiência que o Atleta possui")
+        paralisy = cadastrar_lista(deficiency)
+        covid = validation_str(input("Foi diagnosticado com Covid-19?: [S/N] "), 'SN')
+        sports_quant = validation_int(input("De quantas modalidades o Atleta paticipou: "))
 
-    print("Digite apenas o número da opção correspondente a modalidade que o atleta participou")
-    for i in range(sports_quant):
-        modalidades.append(cadastrar_lista(sports))
+        print("Digite apenas o número da opção correspondente a modalidade que o atleta participou")
+        for i in range(sports_quant):
+            modalidades.append(cadastrar_lista(sports))
 
-    medal = validation_str(input("Ganhou alguma medalha?: [S/N] "), 'SN')
-    if medal == 'S':
-        medal_gold = validation_int(input("Quantas medalhas de ouro: "))
-        medal_silver = validation_int(input("Quantas medalhas de prata: "))
-        medal_bronze = validation_int(input("Quantas medalhas de bonze: "))
-    
-    # lista com os dados
-    dados = [cpf, name, age, gender, paralisy, covid, modalidades, medal, medal_gold, medal_silver, medal_bronze]
+        have_medal = validation_str(input("Ganhou alguma medalha?: [S/N] "), 'SN')
+        if have_medal == 'S':
+            medal['medal_gold'] = validation_int(input("Quantas medalhas de ouro: "))
+            medal['medal_silver'] = validation_int(input("Quantas medalhas de prata: "))
+            medal['medal_bronze'] = validation_int(input("Quantas medalhas de bonze: "))
 
-    # Salvar em um arquivo
-    with open('cadastros.txt', 'a', encoding='utf-8') as cadastrar:
-        cadastrar.write(f'{dados}\n')
+        # criando a classe e salvando no arquivo
+        dados = jp.encode(Atleta(cpf, name, age, gender, paralisy, sports_quant, medal, modalidades))
+        with open('cadastros.json', 'a', encoding='utf-8') as cadastrar:
+            cadastrar.write(f'{dados}\n')
